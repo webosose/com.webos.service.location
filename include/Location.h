f9c57af95a15caabbae3fab11bce18e319052d1e
Vibhanshu Dhote <vibhanshu.dhote@lge.com>
2020-03-02 17:05:05 +0530

    Remove unsupported Location Luna APIs
    :Release Notes:
    Remove Lund registration of unsupported Location Luna APIs
    
    :Detailed Notes:
    Remove unsupported Location Luna APIs from registiring.
    And change the License to Apache.
    
    :Testing Performed:
    Build successful
    unsupported Location Luna APIs not listed in ls-monitor instropection
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-99833] CCC for Location Service and its dependencies
    
    Change-Id: I5b13dde49d59431b3d4bfc8e0ce7f98342ae8928

--- include/Location.h
+++ include/Location.h
@@ -1,25 +1,19 @@
-/********************************************************************
- * Copyright (c) 2012-2013 LG Electronics Inc. All Rights Reserved
- *
- * Project Name : Location framework
- * Group        : PD-4
- * Security     : Confidential
- ********************************************************************/
+// Copyright (c) 2020 LG Electronics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 
-/********************************************************************
- * @File
- * Filename     : Location.h
- * Purpose      : Common location variables & functions
- * Platform     : RedRose
- * Author(s)    : rajesh gopu, Abhishek Srivastava
- * Email ID.    : rajeshgopu.iv@lge.com, abhishek.srivastava@lge.com
- * Creation Date: 14-02-2013
- *
- * Modifications:
- * Sl No        Modified by     Date        Version Description
- *
- *
- ********************************************************************/
 
 #ifndef _LOCATION_H_
 #define _LOCATION_H_
--- src/lunaIpc/LunaLocationServiceUtil.cpp
+++ src/lunaIpc/LunaLocationServiceUtil.cpp
@@ -1,15 +1,19 @@
-/**
- @file      LunaLocationService_Util.cpp
- @brief     Utility File to handle error conditions while parsing JSON objects recieved
- Null Checks on memory allocations, variables recieved in callbacks
+// Copyright (c) 2020 LG Electronics, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 
- @author    Sameer
- @date      2013-03-22
- @version   TDB
- @todo      TDB
-
- @copyright Copyright (c) 2012 LG Electronics.  All rights reserved.
- */
 
 #include "LunaLocationServiceUtil.h"
 #include "ServiceAgent.h"

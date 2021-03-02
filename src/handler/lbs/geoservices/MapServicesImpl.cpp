e5da4f8f9e83b02ada226dbf6b8997dad017a210
Badarinath Nagarala <nagarala.badarinath@lgepartner.com>
2021-03-02 17:25:17 +0530

    Fix coverity issues
    :Release Notes:
    Fix coverity issues in location service
    
    :Detailed Notes:
    using g_free instead of delete operator
    
    :Testing Performed:
    Build successfully.
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-136321] Fix coverity issues in webos
                  components
    [CID 8978869] Incorrect deallocator used
    
    Change-Id: Ic67780d89f6131bbd99be652ef48a68921aa4a11
    Reviewed-on: http://gpro.lge.com/c/webosose/com.webos.service.location/+/305405
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Sameer Mulla <sameer.mulla@lge.com>
    Reviewed-by: vibhanshu dhote <vibhanshu.dhote@lge.com>
    Tested-by: Sameer Mulla <sameer.mulla@lge.com>

--- src/handler/lbs/geoservices/MapServicesImpl.cpp
+++ src/handler/lbs/geoservices/MapServicesImpl.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 LG Electronics, Inc.
+// Copyright (c) 2021 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -207,7 +207,7 @@ string MapServicesImpl::formatUrl(string geoData, std::string url, const char *k
         g_hmac_unref(hmac);
 
     if (encodedSignature != NULL)
-        delete encodedSignature;
+        g_free(encodedSignature);
 
     if (buffer != NULL)
         g_free(buffer);
